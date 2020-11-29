2f0ce6128429bb8dc6ba4a27ba5a5a75bb66e170
snow <rhasya@gmail.com>
2020-11-29 20:02:41 +0900

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,63 +1,41 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import { Button, Card, CardActions, CardContent, Container, makeStyles, TextField, Typography } from '@material-ui/core';
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    width: '100%',
+  },
+  loginBox: {
+    flexGrow: 1,
+    marginTop: theme.spacing(40),
+  },
+  inputBox: {
+    marginTop: theme.spacing(1),
+    width: '100%',
+  },
+  loginBoxBtnArea: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  }
+}))
+
 function App() {
-  const inputUsername = "username"
-  const inputPassword = "password"
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [msg, setMsg] = useState('')
-
-  const handleChange = (e) => {
-    if (e.target.id === inputUsername) {
-      setUsername(e.target.value);
-    }
-    else if (e.target.id === inputPassword) {
-      setPassword(e.target.value);
-    }
-  }
-  const handleSubmit = () => {
-    fetch('/login', {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      method: 'post',
-      body: `username=${username}&password=${password}`})
-        .then(() => { setMsg('login Success') })
-        .catch((err) => { console.error(err) });
-  }
-  const handleSignOut = () => {
-    fetch('/logout');
-  }
-  const handleCheck = () => {
-    fetch('/api/admin/you').then((res) => res.json()).then((data) => console.log(data))
-  }
-  const handleListClick = () => {
-    fetch('/api/app/authors').then((res) => res.json()).then((data) => console.log(data))
-  }
-
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <div>
-        <label htmlFor={inputUsername}>ID</label>
-        <input type="text" id={inputUsername} name={inputUsername} value={username} onChange={handleChange}/>
-      </div>
-      <div>
-        <label htmlFor={inputPassword}>Password</label>
-        <input type="password" id={inputPassword} name={inputPassword} value={password} onChange={handleChange}/>
-      </div>
-      <div>
-        <button onClick={handleSubmit}>Sign in</button>
-        <button onClick={handleSignOut}>Sign out</button>
-        <button onClick={handleCheck}>Check</button>
-      </div>
-
-      <div>
-        <p>{msg}</p>
-        {msg !== '' &&
-        <button onClick={handleListClick}>Get List</button>
-        }
-      </div>
-    </div>
-  );
+    <Container maxWidth="xs" className={classes.root}>
+      <Card className={classes.loginBox}>
+        <CardContent>
+          <Typography variant="h5">Sign in</Typography>
+          <TextField id="username" label="Username" variant="outlined" className={classes.inputBox}/>
+          <TextField id="password" label="Password" variant="outlined" type="password" className={classes.inputBox} autoComplete="off"/>
+        </CardContent>
+        <CardActions className={classes.loginBoxBtnArea}>
+          <Button variant="contained" color="primary">Sign in</Button>
+        </CardActions>
+      </Card>
+    </Container>
+  )
 }
 
 export default App;
